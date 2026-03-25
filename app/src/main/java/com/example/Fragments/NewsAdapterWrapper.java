d725d70bb90a57954fd43bcb7b6375a1eb855f66
mnjprogramacion <mnjprogramacion@gmail.com>
2026-03-25 11:36:46 +0100

    ADD project

---
+++ app/src/main/java/com/example/Fragments/NewsAdapterWrapper.java
@@ -0,0 +1,63 @@
+package com.example.Fragments;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class NewsAdapterWrapper extends BaseAdapter {
+
+    private final Context context;
+    private final NewsBaseAdapter wrappedAdapter;
+    private final int layoutRes;
+
+    public NewsAdapterWrapper(Context context, NewsBaseAdapter wrappedAdapter, int layoutRes) {
+        this.context = context;
+        this.wrappedAdapter = wrappedAdapter;
+        this.layoutRes = layoutRes;
+    }
+
+    @Override
+    public int getCount() {
+        return wrappedAdapter.getCount();
+    }
+
+    @Override
+    public NewsItem getItem(int position) {
+        return wrappedAdapter.getItem(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return wrappedAdapter.getItemId(position);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(layoutRes, parent, false);
+        }
+
+        NewsItem item = wrappedAdapter.getItem(position);
+
+        TextView txtTitulo = convertView.findViewById(R.id.txtTitulo);
+        TextView txtDescripcion = convertView.findViewById(R.id.txtDescripcion);
+        ImageView imgThumbnail = convertView.findViewById(R.id.imgThumbnail);
+
+        txtTitulo.setText(item.titulo);
+        txtDescripcion.setText(item.descripcion);
+
+        if (item.thumbnailUrl != null && !item.thumbnailUrl.isEmpty()) {
+            Picasso.get().load(item.thumbnailUrl)
+                    .placeholder(android.R.drawable.ic_menu_gallery)
+                    .into(imgThumbnail);
+        }
+
+        return convertView;
+    }
+}
