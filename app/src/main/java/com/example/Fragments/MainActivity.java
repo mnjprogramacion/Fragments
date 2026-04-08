1df02fca81930b8e0d6335f02ae7a0c19488b77e
mnjprogramacion <mnjprogramacion@gmail.com>
2026-04-08 10:41:13 +0200

    FIX detail view tablet

--- app/src/main/java/com/example/Fragments/MainActivity.java
+++ app/src/main/java/com/example/Fragments/MainActivity.java
@@ -8,11 +8,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.squareup.picasso.OkHttp3Downloader;
+import com.squareup.picasso.Picasso;
+
+import okhttp3.OkHttpClient;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        OkHttpClient client = new OkHttpClient.Builder().cache(null).build();
+        Picasso picasso = new Picasso.Builder(this)
+                .downloader(new OkHttp3Downloader(client))
+                .build();
+        try {
+            Picasso.setSingletonInstance(picasso);
+        } catch (IllegalStateException ignored) {}
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
